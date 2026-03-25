c1e87ece05a6df241965e6084b97534b7a66bab2
muyiwao <muyiwa.oladimeji@yahoo.com>
2026-03-25 16:50:24 +0000

    updated the vertical loop short script

--- rain_content/scripts/shorts/vertical_shorts_rain_loop_audio_free.py
+++ rain_content/scripts/shorts/vertical_shorts_rain_loop_audio_free.py
@@ -32,7 +32,7 @@ def get_video_info(video_path):
 def extract_quick_vertical():
     # 1. Paths Configuration    
     source_dir = Path(r"C:\Project_Works\YouTubeVideos\video_gen_toolkits\rain_content\recorded\enhanced")
-    output_dir = Path(r"C:\Project_Works\YouTubeVideos\video_gen_toolkits\rain_content\output\output_shorts")
+    output_dir = Path(r"C:\Project_Works\YouTubeVideos\video_gen_toolkits\output\output_shorts")
     output_dir.mkdir(parents=True, exist_ok=True)
     
     img1_path = Path(r"C:\Project_Works\YouTubeVideos\video_gen_toolkits\rain_content\attachments\shorts\subscribe-cta.png")
