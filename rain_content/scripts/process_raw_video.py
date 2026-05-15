c2fc766b185add2eae8d51f974e6c5b2d54dbc3f
muyiwao <muyiwa.oladimeji@yahoo.com>
2026-05-15 15:10:41 +0100

    admin tidy up

---
+++ rain_content/scripts/process_raw_video.py
@@ -0,0 +1,20 @@
+import subprocess
+import json
+import math
+import os
+from pathlib import Path
+import sys
+import re
+
+# Adds the project root to the python path
+root_path = Path(__file__).resolve().parent.parent
+sys.path.append(str(root_path))
+
+from scripts.raw_processing import process_video_length, enhance_rain_atmosphere, overlay_rain_streaks
+
+
+
+if __name__ == "__main__":
+ process_video_length()
+ enhance_rain_atmosphere()
+ overlay_rain_streaks()
